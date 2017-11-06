ed64a7d4c6eacdd5f1d3449ec7a194dab7906224
Robson Fernandes <robson.ux@gmail.com>
2017-11-06 00:36:53 -0200

    Organização dos arquivos

--- Sistemas Complex I/Trabalho II/teste.R
+++
@@ -1,6 +0,0 @@
-library(igraph)
-size = 6
-
-# tree graph with two children for each node
-g = graph.tree(size, children = 2, mode = "undirected")
-plot(g, layout=layout_with_fr, vertex.color ="Green")
